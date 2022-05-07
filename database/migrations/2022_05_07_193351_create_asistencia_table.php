9740f8357031057a1b41ffc3590e110032b8862e
GuillermoCervLobo <A264644@alumnos.uaslp.mx>
2022-05-07 17:52:12 -0500

    Migrations

--- database/migrations/2022_05_07_193351_create_asistencia_table.php
+++ database/migrations/2022_05_07_193351_create_asistencia_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class Asistencia extends Migration
 {
     /**
      * Run the migrations.
@@ -13,11 +13,11 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('asistencia', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
+            $table->foreignId('medico_id');
+            $table->foreignId('paciente_id');
+            $table->timestamp('fecha'); //con hora
             $table->timestamps();
         });
     }
@@ -29,6 +29,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        //
     }
 }
