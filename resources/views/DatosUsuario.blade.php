e42fe854c16c1bba22e01a002b4bed9804fb04ee
Isai-1410 <78745884+Isai-1410@users.noreply.github.com>
2022-05-17 08:48:16 -0500

    Datos usuario y medico

---
+++ resources/views/DatosUsuario.blade.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+  </head>
+    <title>Datos del usuario</title>
+
+    <link rel="stylesheet" href="css/datosusuario.css">
+</head>
+<body>
+    
+    <div class="Data">
+        <b><h1>Datos del usuario</h1></b><br>
+        <label>Nombre del paciente</label><br>
+        <input type="text" value="Nombre y apellido" class="boxes"> <img src="pencil-square.svg" alt=""><div class="edit">
+            </div><br>
+        <br>
+        <label>Edad</label><br>
+        <input type="text" value="Edad del paciente"class="boxes"><img src="pencil-square.svg" alt=""><div class="edit">
+            
+            </div><br>
+        <br>
+        <label>Lugar de origen</label><br>
+        <input type="text" value="Origen" class="boxes"><img src="pencil-square.svg" alt=""><div class="edit">
+
+            </div><br>
+        <br>
+        <label>Peso</label><br>
+        <input type="text" value="Peso" class="boxes"><img src="pencil-square.svg" alt=""><div class="edit">
+           
+            </div><br>
+        <br>
+        <label>Estatura</label><br>
+        <input type="text" value="Estatura" class="boxes"><img src="pencil-square.svg" alt=""><div class="edit">
+            
+            </div><br>
+        <br>
+        <button type="button" class="btn btn-primary">Guardar Cambios</button>
+        
+    </div>
+    <i class="fa-solid fa-user"></i>
+
+    
+
+
+    
+</body>
+</html>
