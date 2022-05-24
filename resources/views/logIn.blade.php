f974a724c3b9bfca243344f78e59fc0286c6c14e
Jovan Lopez <49563725+jovanlopez32@users.noreply.github.com>
2022-05-24 00:06:55 -0500

    Update logIn.blade.php

--- resources/views/logIn.blade.php
+++ resources/views/logIn.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Iniciar Sesión</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.2/font/bootstrap-icons.css">
-    <link rel="stylesheet" href="assets/css/logInStyles.css"/>
+    <link rel="stylesheet" href="css/logInStyles.css"/>
 </head>
 
 <body>
