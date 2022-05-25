97f363b969164161ef7f3362e0ada92f644d2d1d
JPabloRdz179 <75864360+JPabloRdz179@users.noreply.github.com>
2022-05-25 14:46:37 -0500

    Se crearon dos archivos a las vistas
    Representan dos navbars, uno para cada tipo de usuario.

--- resources/views/asistePacientesMedico.blade.php
+++ resources/views/asistePacientesMedico.blade.php
@@ -25,29 +25,7 @@
 
 <!--Esta pantalla es parte del flujo del Medico, en específico cuando presiona el botón de "Pacientes"-->
 
-<body>
-    <div class="contNavbar">
-        <div class="pos-f-t navbarPaciente">
-            <div id="nav1">
-                <div class="dropdown">
-                    <i class="fa-solid fa-bars fa-2x boton-nav " id="username dropdownMenu2" href="#"
-                        data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"></i>
-                    <div class="dropdown-menu menuoption" aria-labelledby="dropdownMenu2">
-                        <button class="dropdown-item menuoption" type="button" href="index.html">Cerrar Sesión</button>
-                        <button class="dropdown-item menuoption" type="button">Ver Perfíl</button>
-                        <button class="dropdown-item menuoption" type="button">Saber más</button>
-                    </div>
-                </div>
-                <a href="">
-                    <h6>Médicos</h6>
-                </a>
-
-            </div>
-            <img src="{{url('/images/uaslp-vector-logo.png')}}" alt="Image"/>
-        </div>
-        <div class="lineaNav"></div>
-    </div>
-    
+@include('navbar')
 
   <div class="contenedor" id="uno">
         <div class="contenido">
