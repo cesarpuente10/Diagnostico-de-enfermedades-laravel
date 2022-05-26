8078d88b29173805cfb05d0a2e177ef8f20720f5
GuillermoCervLobo <A264644@alumnos.uaslp.mx>
2022-05-25 21:47:17 -0500

    Deleted medicos y pacientes

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,8 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->string('password');
             $table->integer('role');
+            $table->string('especialidad')->nullable();
+            $table->string('cedula')->nullable();
             $table->timestamps();
         });
     }
