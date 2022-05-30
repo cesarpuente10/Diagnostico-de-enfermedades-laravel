9d7b6232041f1ca6aa2a4ef380ba36571e9deec4
Isai-1410 <78745884+Isai-1410@users.noreply.github.com>
2022-05-30 18:37:05 -0500

    new

---
+++ resources/views/DatosUsuarioNoeditable.blade.php
@@ -0,0 +1,32 @@
+<x-app-layout>
+
+        @csrf
+        <b><h1 class="DatoUsuario">Datos del usuario</h1></b><br>
+        <label>Nombre del paciente</label><br>
+        <input type="text" placeholder="Nombre y apellido" class="boxes"> <img src="pencil-square.svg" alt=""><div class="edit">
+            </div><br>
+        <br>
+        <label>Edad</label><br>
+        <input type="text" placeholder="Edad del paciente"class="boxes"><img src="pencil-square.svg" alt=""><div class="edit">
+            
+            </div><br>
+        <br>
+        <label>Lugar de origen</label><br>
+        <input type="text" placeholder="Origen" class="boxes"><img src="pencil-square.svg" alt=""><div class="edit">
+
+            </div><br>
+        <br>
+        <label>Peso</label><br>
+        <input type="text" placeholder="Peso" class="boxes"><img src="pencil-square.svg" alt=""><div class="edit">
+           
+            </div><br>
+        <br>
+        <label>Estatura</label><br>
+        <input type="text" placeholder="Estatura" class="boxes"><img src="pencil-square.svg" alt=""><div class="edit">
+            
+            </div><br>
+        <br>
+        <input type="submit" class="btn btn-primary" value="Guardar Cambios">
+        
+
+</x-app-layout>
