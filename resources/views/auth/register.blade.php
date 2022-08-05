f796f279941445dd7d58cbfa1851de0aee6c0ce3
JPabloRdz179 <75864360+JPabloRdz179@users.noreply.github.com>
2022-08-05 04:25:22 -0500

    Registro-Diseño
    Muestra un formulario a la vez y la checkbox ya se ve
    Trabajo entre Pablo y Antonio

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,36 +1,36 @@
 <x-guest-layout>
-    
-    <div class="info-flex">
-        <p>
-        <button  class="info" class="btn btn-info" type="button" data-bs-toggle="collapse" data-bs-target="#multiCollapseExample1" aria-expanded="false" aria-controls="multiCollapseExample1">Registro Médico</button>
-        <button  class="info" class="btn btn-info" type="button" data-bs-toggle="collapse" data-bs-target="#multiCollapseExample2" aria-expanded="false" aria-controls="multiCollapseExample2">Registro Paciente</button>
-    </div>
+    <?php 
+    $bandera = 0;
+    ?>
+    <div class="registro">
+        <div class="info-flex">
+            <p>
+            <!--<button class="info" type="button" data-bs-toggle="collapse" data-bs-target="#multiCollapseExample1" aria-expanded="false" aria-controls="multiCollapseExample1">Registro Médico</button>-->
+            <button class="info" type="button" data-bs-toggle="collapse" data-bs-target="#multiCollapseExample1" aria-expanded="false" aria-controls="multiCollapseExample1" onclick="window.location.href='register?flag=2'">Registro Médico</button>
+            <button class="info" type="button" data-bs-toggle="collapse" data-bs-target="#multiCollapseExample2" aria-expanded="false" aria-controls="multiCollapseExample2" onclick="window.location.href='register?flag=1'">Registro Paciente</button>
+        </div>
 
-    
-
-    <div class="row">
-
-        <div class="col">
-            <div class="collapse multi-collapse" id="multiCollapseExample1">
-                <div class="card card-body">
-                    <div class="content">
+        <?php 
+            if(!empty($_GET))
+            {
+                $bandera = $_GET['flag'];
+                if($bandera=="2"){
+                    ?>
+                    <div class="content-reg">
                         <section class="register">
-                            <h1>Registro Médico</h1>
+                            <h1>Registro Medico</h1>
                             <form method="POST" action="{{ route('register') }}">
-                                @csrf
-                                
+                            @csrf
+                            
                                 <input type="hidden" name="role" value="2">
 
                                 <label for="name">Nombre(s)</label>
                                 <input type="text" id="name" name="name">
 
-                                <label for="lastnamef">Apellido paterno</label>
-                                <input type="text" id="lastnamef" name="lastnamef">
+                                <label for="apellido">Apellidos</label>
+                                <input type="text" id="apellido">
 
-                                <label for="lastnamem">Apellido materno</label>
-                                <input type="text" id="lastnamem" name="lastnamem">
-
-                                <label for="correo">Correo electrónico</label>
+                                <x-label for="email" :value="__('Correo Electrónico')" />
                                 <input type="email" id="email" name="email">
 
                                 <label for="pass">Contraseña</label>
@@ -43,38 +43,29 @@
                                 <input type="text" id="cert" name="cert">
 
                                 <div>
-                                    <input type="checkbox" id="check">
-                                    <label id="size" for="check">Acepto Terminos y Condiciones</label>
+                                <input type="checkbox" id="check">
+                                <label id="size" for="check">Acepto Terminos y Condiciones</label>
                                 </div>
-
                                 <button type="submit">{{ __('Registrar') }}</button>
                             </form>
                         </section>
                     </div>
-                </div>
-            </div>
-        </div>
-
-        
-        <div class="col">
-            <div class="collapse multi-collapse" id="multiCollapseExample2">
-                <div class="card card-body">
-                    <div class="content">
+                    <?php
+                        //echo "<meta http-equiv='refresh' content='0;url=./logica/deletetodoCarrito.php?id=$idUsuario'>";
+                }elseif($bandera=="1"){
+                    ?>
+                    <div class="content-reg">
                         <section class="register">
                             <h1>Registro Paciente</h1>
                             <form method="POST" action="{{ route('register') }}">
-                                @csrf
-
+                            @csrf
                                 <input type="hidden" name="role" value="1">
 
                                 <label for="name">Nombre(s)</label>
                                 <input type="text" id="name" name="name">
 
-                                <label for="lastnamef">Apellido paterno</label>
-                                <input type="text" id="lastnamef" name="lastnamef">
-
-                                <label for="lastnamem">Apellido materno</label>
-                                <input type="text" id="lastnamem" name="lastnamem">
+                                <label for="apellido">Apellidos</label>
+                                <input type="text" id="apellido">
 
                                 <label for="correo">Correo electrónico</label>
                                 <input type="email" id="email" name="email">
@@ -86,19 +77,21 @@
                                 <input type="password" id="password_confirmation" name="password_confirmation">
 
                                 <div>
-                                    <input type="checkbox" id="check">
-                                    <label id="size" for="check">Acepto Terminos y Condiciones</label>
+                                <input type="checkbox" id="check">
+                                <label id="size" for="check">Acepto Terminos y Condiciones</label>
                                 </div>
-
                                 <button type="submit">{{ __('Registrar') }}</button>
                             </form>
                         </section>
                     </div>
-                </div>
-            </div>
-        </div>
-
+                    <?php
+                }
+                
+            }
+            
+        ?>
     </div>
+    
 
 
 
