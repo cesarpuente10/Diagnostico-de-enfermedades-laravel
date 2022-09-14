69e105f6d7162223b144861a2a3aee9fbc640aae
GuillermoCervLobo <A264644@alumnos.uaslp.mx>
2022-09-14 11:38:29 -0500

    Base correcta de pantalla de medico

--- resources/views/PerfilMedico.blade.php
+++ resources/views/PerfilMedico.blade.php
@@ -1,20 +1,8 @@
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-        <link rel="stylesheet" href="/perfil.css">
-    <title>Perfil del Médico</title>
-</head>
-
-<body>
+<x-app-layout>
 
     <div class="center">
     <div>
-        <img src="perfil.png" width="200px">
+        <img src="{{url('/images/perfil.png')}}" width="200px">
     </div><br><br>
 
     
@@ -97,8 +85,4 @@
     </form>
 </div>
 
-</body>
-
-</html>
-
-<!--<x-app-layout></x-app-layout>
+</x-app-layout>
